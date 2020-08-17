a90c014d58386992ad5b9cb3aa4b120c58678928
shoko-tzw <69617989+shoko-tzw@users.noreply.github.com>
2020-08-17 17:41:34 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+	
+$(window).on("scroll",function(){
+		if($(this).scrollTop()>100){
+			$(".pagetop").fadeIn();
+		}else{
+			$(".pagetop").fadeOut();
+		}
+	});
+
+$(function() {
+	function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('js-hamburger');
+  var blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
+	});	
